025b90bdc3d6aa722140b4393f30beb4c9546e9c
pranathichiluvuri-ops <pranathi.chiluvuri@gmail.com>
2026-07-20 16:26:55 -0700

    print true if the given number is divisible 2,3,5

---
+++ UglyNumbersFromLeetCode.py
@@ -0,0 +1,11 @@
+a = int(input())
+if a == 1:
+    print("True")
+elif a % 2 == 0:
+    print("True")
+elif a % 3 == 0:
+    print("True")
+elif a % 5 == 0:
+    print("Ture")
+else:
+    print("Flase")
