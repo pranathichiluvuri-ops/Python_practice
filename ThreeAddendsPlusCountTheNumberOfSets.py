f3fad63a71bdf68a79290b3a1a41de5462d59cd4
pranathichiluvuri-ops <pranathi.chiluvuri@gmail.com>
2026-08-20 12:28:22 -0700

    Input - 1 1 2 3 4 /next line 4 output - Addends = 1 1 2 /next line No.of sets = 1

---
+++ ThreeAddendsPlusCountTheNumberOfSets.py
@@ -0,0 +1,11 @@
+a = list(map(int, input().split()))
+b = int(input())
+l = len(a)
+count = 0
+for i in range(l):
+    for j in range(i + 1, l):
+        for k in range (j + 1, l):
+            if a[i] + a[j] + a[k] == b:
+                print("Addends =", a[i], a[j], a[k])
+                count = 1 + count
+print("No.of sets =", count)
